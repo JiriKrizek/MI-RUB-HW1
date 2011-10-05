364f7c149fbd7d0ec2aa22ff01b183c779c438b6
JiriKrizek <jkrizek@gmail.com>
2011-10-05 22:56:23 +0200

    Parsovani a debug vypis

--- src/Parser.rb
+++ src/Parser.rb
@@ -1,6 +1,6 @@
 
 class Parser
-  attr_reader :input, :graphs_no
+  attr_reader :input, :graphs_no, :type
 
   DEBUG=true
   
@@ -18,10 +18,47 @@ class Parser
 
   def parseInput
     lines = @input.split(/\n/)
-    p lines[0]
+    lineno=0
 
-    @graphs_no = lines[0]
+    @graphs_no = lines[lineno].to_i
+    lineno+=1
     puts "Graphs # set to #{@graphs_no}" if DEBUG
+
+    # Go through graphs
+    (1..@graphs_no).each do |graph|
+      puts "======================================================="
+      puts "Graph #{graph}, lineno: #{lineno} ::: #{lines[lineno]}"
+      records = lines[lineno].to_i
+      puts "  Records #{records}"
+      
+      # Go through records in one graph
+      start = lineno-1
+      (lineno..lineno+records-1).each do |line|
+        lineno += 1
+        puts "    Record: #{line-start}: #{lines[lineno]}"
+      end
+
+      lineno+=1
+
+      puts ""
+
+      # Go through search requests
+      while !lines[lineno].eql?("0 0")
+        num = lines[lineno].split(" ")
+        print "    Search: #{num[0]} using "
+        if(num[1].to_i==0)
+          puts "DFS"
+        else 
+          puts "BFS"
+        end
+
+        lineno+=1
+      end
+      lineno+=1
+    end
+
+
+
   end
 end
 
